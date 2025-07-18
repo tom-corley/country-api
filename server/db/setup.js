8dd15069c61ff61cbc4b5f81f4a751f498e72fc6
Tom Corley <tomcorley86@gmail.com>
2025-07-18 10:02:15 +0100

    Adding comments.

--- server/db/setup.js
+++ server/db/setup.js
@@ -1,8 +1,10 @@
+// Dependencies for reading environment variables and file system 
+// and connecting to the database
 require("dotenv").config();
 const fs = require("fs");
-
 const db = require("./connect");
 
+// Read the SQL file that contains the database setup commands
 const sql = fs.readFileSync("./server/db/countries.sql").toString();
 
 // Run the query to set up the database
